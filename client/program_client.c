c6f6c1b6a34345d813f82ca405b77222faa758b8
Fayha Syifa Qalbi <72442637+sfayhaa@users.noreply.github.com>
2021-06-18 18:10:01 +0700

    Yang fix yang ini aja han

--- client/program_client.c
+++ client/program_client.c
@@ -11,3 +11,36 @@
 
 #define PORT 8080
 #define DATA_BUFFER 1024
+
+int main(int argc, char const *argv[]) {
+    struct sockaddr_in address;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char buffer[DATA_BUFFER] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+  
+    memset(&serv_addr, '0', sizeof(serv_addr));
+  
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+      
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+  
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    send(sock , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+    valread = read( sock , buffer, DATA_BUFFER);
+    printf("%s\n",buffer );
+    return 0;
+}
